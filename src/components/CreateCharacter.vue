7e30c8d3e86c590e6d448a603d3123e06d4cb28a
Mateus Koppe <mateus.koppe@gmail.com>
2018-04-10 20:44:44 -0300

    Improve CreateCharacter form

--- src/components/CreateCharacter.vue
+++ src/components/CreateCharacter.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     <h2>Character</h2>
-    <select>
-        <option value="" v-for="race in races" :key="race.name">{{race.name}}</option>
-    </select>
+    <div>
+      <label for="race">Race:</label>
+      <select v-model="form.race" id="race">
+          <option v-for="race in races" :key="race.name">{{race.name}}</option>
+      </select>
+    </div>
   </div>
 </template>
 
@@ -13,6 +16,7 @@ import races from '@/data/races'
 export default {
   data () {
     return {
+      form: {},
       races
     }
   }
