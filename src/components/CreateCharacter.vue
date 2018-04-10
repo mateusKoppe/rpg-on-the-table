92c39739acb8725fc618697540e5ac527a7836cf
Mateus Koppe <mateus.koppe@gmail.com>
2018-04-10 20:49:14 -0300

    Add subRaces

--- src/components/CreateCharacter.vue
+++ src/components/CreateCharacter.vue
@@ -2,9 +2,19 @@
   <div>
     <h2>Character</h2>
     <div>
-      <label for="race">Race:</label>
+      <label for="race">Race: </label>
       <select v-model="form.race" id="race">
-          <option v-for="race in races" :key="race.name">{{race.name}}</option>
+          <option v-for="race in races" :value="race" :key="race.name">{{race.name}}</option>
+      </select>
+    </div>
+    <div v-if="form.race && form.race.subRaces">
+      <label for="race">Race: </label>
+      <select v-model="form.subRace" id="race">
+          <option
+            v-for="subRace in form.race.subRaces"
+            :value="subRace"
+            :key="subRace.name"
+          >{{subRace.name}}</option>
       </select>
     </div>
   </div>
--- src/data/races.js
+++ src/data/races.js
@@ -1,12 +1,39 @@
 export default {
   dwarf: {
-    name: 'Dwarf'
+    name: 'Dwarf',
+    subRaces: {
+      hill: {
+        name: 'Hill Dwarf'
+      },
+      mountain: {
+        name: 'Mountain Dwarf'
+      }
+    }
   },
   elf: {
-    name: 'Elf'
+    name: 'Elf',
+    subRaces: {
+      dark: {
+        name: 'Dark Elf'
+      },
+      high: {
+        name: 'High Elf'
+      },
+      wood: {
+        name: 'Wood Elf'
+      }
+    }
   },
   halfling: {
-    name: 'Halfling'
+    name: 'Halfling',
+    subRaces: {
+      lightfoot: {
+        name: 'Lightfoot'
+      },
+      stout: {
+        name: 'Stout'
+      }
+    }
   },
   human: {
     name: 'Human'
@@ -15,7 +42,15 @@ export default {
     name: 'Dragonborn'
   },
   gnome: {
-    name: 'Gnome'
+    name: 'Gnome',
+    subRaces: {
+      forest: {
+        name: 'Forest Gnome'
+      },
+      rock: {
+        name: 'Rock Gnome'
+      }
+    }
   },
   halfElf: {
     name: 'Half-Elf'
