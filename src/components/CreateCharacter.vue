18529244886cc070b2de4a661520f13c26d11d76
Mateus Koppe <mateus.koppe@gmail.com>
2018-04-10 21:09:50 -0300

    Fix createCharacter fields id

--- src/components/CreateCharacter.vue
+++ src/components/CreateCharacter.vue
@@ -4,28 +4,28 @@
     <div>
       <label for="race">Race: </label>
       <select v-model="form.race" id="race">
-          <option v-for="race in races" :value="race" :key="race.name">{{race.name}}</option>
+        <option v-for="race in races" :value="race" :key="race.name">{{race.name}}</option>
       </select>
     </div>
     <div v-if="form.race && form.race.subRaces">
-      <label for="race">Race: </label>
-      <select v-model="form.subRace" id="race">
-          <option
-            v-for="subRace in form.race.subRaces"
-            :value="subRace"
-            :key="subRace.name"
-          >{{subRace.name}}</option>
+      <label for="subRace">SubRace: </label>
+      <select v-model="form.subRace" id="subRace">
+        <option
+          v-for="subRace in form.race.subRaces"
+          :value="subRace"
+          :key="subRace.name"
+        >{{subRace.name}}</option>
       </select>
     </div>
     <div>
-      <label for="race">Classe: </label>
-      <select v-model="form.class" id="race">
+      <label for="class">Classe: </label>
+      <select v-model="form.class" id="class">
         <option v-for="classe in classes" :value="classe" :key="classe.name">{{classe.name}}</option>
       </select>
     </div>
     <div>
-      <label for="race">Background: </label>
-      <select v-model="form.background" id="race">
+      <label for="background">Background: </label>
+      <select v-model="form.background" id="background">
         <option v-for="background in backgrounds"
           :value="background"
           :key="background.name"
