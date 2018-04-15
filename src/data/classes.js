98e8901910afe70dfb2bd1bf4d4848402259de2b
Mateus Koppe <mateus.koppe@gmail.com>
2018-04-15 15:00:02 -0300

    Add barbarian information

--- src/data/classes.js
+++ src/data/classes.js
@@ -1,6 +1,47 @@
 export default {
   barbarian: {
-    name: 'Barbarian'
+    hitDice: '1d12',
+    hitPointAt1thLevel () {
+      return 12 + this.abilities.con.mod
+    },
+    hitPointAtHighestLevels () {
+      return this.baseLifePoints + this.abilities.con.mod * (this.actualLevel - 1)
+    },
+    levels: {
+      1: {
+        proficiencyBonus: 2,
+        features: {
+          rage: {
+            name: 'Rage',
+            description: 'Lorem ipsum'
+          },
+          unamoredDefense: {
+            name: 'Unamored Defense',
+            description: 'Lorem ipsum'
+          }
+        }
+      }
+    },
+    name: 'Barbarian',
+    proficiencies: {
+      armor: ['lightArmor', 'mediumArmor', 'shields'],
+      tools: [],
+      savingThrows: ['str', 'con'],
+      skillsToChoose: [
+        {
+          pick: 2,
+          of: [
+            'animalHandling',
+            'athletics',
+            'intimidation',
+            'nature',
+            'perception',
+            'survival'
+          ]
+        }
+      ],
+      weapons: ['simpleWeapon', 'martialWeapon']
+    }
   },
   bard: {
     name: 'Bard'
