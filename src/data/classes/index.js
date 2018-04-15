8df10bf9c6cd773adde808568a5e980d5ff23640
Mateus Koppe <mateus.koppe@gmail.com>
2018-04-15 15:11:30 -0300

    Create a file por each class

---
+++ src/data/classes/index.js
@@ -0,0 +1,27 @@
+import barbarian from './barbarian'
+import bard from './bard'
+import cleric from './cleric'
+import druid from './druid'
+import fighter from './fighter'
+import monk from './monk'
+import paladin from './paladin'
+import ranger from './ranger'
+import rogue from './rogue'
+import sorcerer from './sorcerer'
+import warlock from './warlock'
+import wizard from './wizard'
+
+export default {
+  barbarian,
+  bard,
+  cleric,
+  druid,
+  fighter,
+  monk,
+  paladin,
+  ranger,
+  rogue,
+  sorcerer,
+  warlock,
+  wizard
+}
