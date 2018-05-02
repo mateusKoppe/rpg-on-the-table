947c7621cc0e5fb2477be1426a8efa86d41ac159
Mateus Koppe <mateus.koppe@gmail.com>
2018-05-01 22:53:14 -0300

    Create ChoicesList component

---
+++ src/components/ChoicesList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div v-if="choices.pick === 1">
+      <ChoicesListSingle
+        :choices="choices"
+        v-model="selected"
+      />
+    </div>
+    <div v-else>
+      {{choices}}
+    </div>
+    <div v-if="selectedIsChoice">
+      <ChoicesList :choices="selected.value" />
+    </div>
+    <div v-else>{{selected}}</div>
+  </div>
+</template>
+
+<script>
+import ChoicesListSingle from './ChoicesListSingle'
+
+export default {
+  name: 'ChoicesList',
+
+  props: {
+    choices: Object
+  },
+
+  components: {
+    ChoicesListSingle
+  },
+
+  data () {
+    return {
+      selected: null
+    }
+  },
+
+  computed: {
+    selectedIsChoice () {
+      return (
+        this.selected &&
+        this.selected.value &&
+        this.selected.value.role === 'choice'
+      )
+    }
+  }
+}
+</script>
+
---
+++ src/components/ChoicesListSingle.vue
@@ -0,0 +1,32 @@
+<template>
+  <select v-model="selected">
+    <option
+      v-for="(option, index) in choices.of"
+      :key="index"
+      :value="option"
+    >{{option.name || option}}</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'ChoicesListSingle',
+
+  props: {
+    choices: Object
+  },
+
+  data () {
+    return {
+      selected: null
+    }
+  },
+
+  watch: {
+    selected (value) {
+      this.$emit('input', value)
+    }
+  }
+}
+</script>
+
