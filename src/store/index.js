0aa51755be254835e4f64cc505a96fe3aa77c5b1
Mateus Koppe <mateus.koppe@gmail.com>
2018-06-16 19:48:15 -0300

    feat(store): add actualCharacter

--- src/store/index.js
+++ src/store/index.js
@@ -5,16 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    characters: []
+    characters: [],
+    actualCharacter: null
   },
   getters: {
     characters (state) {
       return state.characters
+    },
+    actualCharacter (state) {
+      return state.actualCharacter
     }
   },
   mutations: {
     addCharacter (state, characters) {
       state.characters.push(characters)
+    },
+    setActualCharacter (state, characters) {
+      state.actualCharacter = characters
     }
   }
 })
