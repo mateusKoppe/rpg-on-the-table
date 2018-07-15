3cba46adc8408fdb699176e6fa9fd6c3f0d387bf
Mateus Koppe <mateus.koppe@gmail.com>
2018-07-14 21:12:25 -0300

    Change inputs

--- src/components/CharacterCreateBasis.vue
+++ src/components/CharacterCreateBasis.vue
@@ -1,49 +1,39 @@
 <template>
 <div>
-  <div>
-    <label for="name">Name: </label>
-    <input v-model="form.name" id="name"/>
-  </div>
-  <div>
-    <label for="race">Race: </label>
-    <select v-model="form.race" id="race">
-      <option
-        v-for="(race, key) in races"
-        :value="key"
-        :key="key"
-      >{{race.name}}</option>
-    </select>
-  </div>
-  <div v-if="form.race && races[form.race].subRaces">
-    <label for="subRace">SubRace: </label>
-    <select v-model="form.subRace" id="subRace">
-      <option
-        v-for="(subRace, key) in races[form.race].subRaces"
-        :value="key"
-        :key="key"
-      >{{subRace.name}}</option>
-    </select>
-  </div>
-  <div>
-    <label for="class">Classe: </label>
-    <select v-model="form.class" id="class">
-      <option
-        v-for="(classe, index) in classes"
-        :value="index"
-        :key="index"
-      >{{classe.name}}</option>
-    </select>
-  </div>
-  <div>
-    <label for="background">Background: </label>
-    <select v-model="form.background" id="background">
-      <option
-        v-for="(background, key) in backgrounds"
-        :value="key"
-        :key="key"
-      >{{background.name}}</option>
-    </select>
-  </div>
+  <VInput label="Nome" v-model="form.name"/>
+  <VSelect
+    label="Race"
+    :list="races"
+    optionvalue="key"
+    optionkey="key"
+    optionLabel="name"
+    v-model="form.race"
+  />
+  <VSelect
+    v-if="form.race && races[form.race].subRaces"
+    label="SubRace"
+    :list="races[form.race].subRaces"
+    optionvalue="key"
+    optionkey="key"
+    optionLabel="name"
+    v-model="form.subRace"
+  />
+  <VSelect
+    label="Class"
+    :list="classes"
+    optionvalue="key"
+    optionkey="key"
+    optionLabel="name"
+    v-model="form.class"
+  />
+  <VSelect
+    label="Background"
+    :list="backgrounds"
+    optionvalue="key"
+    optionkey="key"
+    optionLabel="name"
+    v-model="form.background"
+  />
 </div>
 </template>
 
