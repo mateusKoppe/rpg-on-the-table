1553041189134b354cf1f418ebfae1a63c891537
Mateus Koppe <mateus.koppe@gmail.com>
2020-02-10 17:59:00 -0300

    feat(equipment): implement model in equipmentitem

--- src/components/EquipmentItem.vue
+++ src/components/EquipmentItem.vue
@@ -1,36 +1,45 @@
 <template>
-<VCard class="EquipmentItem">
-  <h3 class="EquipmentItem__name">{{value.name}}</h3>
-  {{ value.note }}
-</VCard>
+<div>
+  <div @click="showInfo()">
+    <VCard class="EquipmentItem">
+      <h3 class="EquipmentItem__name">{{value.name}}</h3>
+      {{ value.note }}
+    </VCard>
+  </div>
+  <VModal ref="equipmentInfoModal">
+    <EquipmentForm v-model="equipment"></EquipmentForm>
+  </VModal>
+</div>
 </template>
 
 <script>
+import EquipmentForm from "@/components/EquipmentForm";
+
 export default {
   name: 'EquipmentItem',
+
+  components: {
+    EquipmentForm
+  },
 
   props: {
     value: Object
   },
 
-  data () {
-    return {
-      equipment: {
-        name: "",
-        weight: 0
+  computed: {
+    equipment: {
+      get () {
+        return {...this.value}
+      },
+      set (data) {
+        this.$emit('input', {...data})
       }
     }
   },
 
-  watch: {
-    equipment: {
-      handler (value) {
-        this.$emit('input', {
-          ...value,
-          weight: Number(value.weight),
-        })
-      },
-      deep: true
+  methods: {
+    showInfo () {
+      this.$refs.equipmentInfoModal.open()
     }
   }
 }
